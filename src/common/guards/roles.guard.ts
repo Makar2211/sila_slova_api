4f3d6697944a83d2b4bd208169bcd2427f50a140
Makar <makardovgopolji@gmail.com>
2026-04-07 15:11:21 +0200

    feature: auth

---
+++ src/common/guards/roles.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable, CanActivate, ExecutionContext, ForbiddenException, UnauthorizedException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+import { Role } from '../../../prisma/generated/prisma/enums';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!requiredRoles) return true;
+
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+
+    if (!user) throw new UnauthorizedException();
+    if (!requiredRoles.includes(user.role)) throw new ForbiddenException('Access denied');
+
+    return true;
+  }
+}
